7d405ffa68b008856dfbeaf626a62c24a2ccdee6
Edgar <mr.edge27@gmail.com>
2022-09-19 11:18:00 -0500

    fix - on headers

--- api/app.js
+++ api/app.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers','authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
-    res-header('Access-Control-Allow-Methods', 'GET, POST , OPTIONS , PUT, DELETED');
-    res-header('Allow', 'GET, POST , OPTIONS , PUT, DELETED');
+    res.header('Access-Control-Allow-Methods', 'GET, POST , OPTIONS , PUT, DELETED');
+    res.header('Allow', 'GET, POST , OPTIONS , PUT, DELETED');
     next();
 });
 
